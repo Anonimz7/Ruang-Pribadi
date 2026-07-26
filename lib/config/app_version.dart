5eb84c3df1d538a7c4432596fd82b9852bbec4eb
anonimzx7 <anonimzx7@gmail.com>
2026-07-26 15:21:57 +0700

    udpate 1.4.0

--- lib/config/app_version.dart
+++ lib/config/app_version.dart
@@ -5,6 +5,6 @@
 ///   1.1.0 → 2
 ///   2.0.0 → 3
 class AppVersion {
-  static const int code = 4;
-  static const String name = '1.3.0'; ///fix backup
+  static const int code = 5;
+  static const String name = '1.4.0'; /// fix sitemap
 }
