09eaf96a4da5ae0a31f17c5156cb7ea007841e49
anonimzx7 <anonimzx7@gmail.com>
2026-08-04 14:55:01 +0700

    1.6.0 build 7

--- lib/config/app_version.dart
+++ lib/config/app_version.dart
@@ -5,6 +5,6 @@
 ///   1.1.0 → 2
 ///   2.0.0 → 3
 class AppVersion {
-  static const int code = 6;
-  static const String name = '1.5.0'; /// add fitur baru pengkategorian
+  static const int code = 7;
+  static const String name = '1.6.0'; /// fix stock_list
 }
